a417df317c49bed3e12107f42ff639cb1244c0fb
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-05-28 02:26:07 +0200

    User inputs collected. Data structure organized.

--- app.js
+++ app.js
@@ -1,16 +1,67 @@
+// BUDGET CONTROLLER
 var budgetController =  (function() {
 
+    // Some code
 
 })();
 
+
+// UI CONTROLLER
 var UIController = (function() {
 
-    // Some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        description: '.add__description',
+        value: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.description).value,
+                value: document.querySelector(DOMstrings.value).value
+            }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
 
 })();
 
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCTrl, UICtrl) {
 
-    // Some code
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function() {
+
+        // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input)
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5 . Display the budget on the UI
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.witch === 13) {
+            
+            ctrlAddItem();
+        }
+
+    });
 
 })(budgetController, UIController);
