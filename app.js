22196230a5add599ad3dce738e278b641ebfc822
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-05-29 00:50:57 +0200

    Init function implemented

--- app.js
+++ app.js
@@ -36,7 +36,21 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCTrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+
+            if (event.keyCode === 13 || event.witch === 13) {
+            
+            ctrlAddItem();
+            }
+        });
+    };
+
+    
 
     var ctrlAddItem = function() {
 
@@ -51,17 +65,13 @@ var controller = (function(budgetCTrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5 . Display the budget on the UI
+    };
+    return {
+        init: function() {
+            setupEventListeners();
+        }
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-
-        if (event.keyCode === 13 || event.witch === 13) {
-            
-            ctrlAddItem();
-        }
-
-    });
-
 })(budgetController, UIController);
+
+controller.init();
