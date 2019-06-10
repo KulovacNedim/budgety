0727e3acae98d28ee472255e4b96475a7d6441ca
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-06-10 11:48:33 +0200

    Formating and displahying date on UI implemented

--- app.js
+++ app.js
@@ -138,7 +138,8 @@ var UIController = (function () {
     expensesLabel: '.budget__expenses--value',
     pecentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   };
 
   var formatNumber= function(num, type) {
@@ -253,6 +254,19 @@ var UIController = (function () {
       });
     },
 
+    displayMonth: function() {
+      var now, month, months, year;
+
+      now = new Date();
+
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = now.getMonth();
+
+      year = now.getFullYear();
+
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     }
@@ -352,6 +366,7 @@ var controller = (function (budgetCtrl, UICtrl) {
   return {
     init: function () {
       setupEventListeners();
+      UIController.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
